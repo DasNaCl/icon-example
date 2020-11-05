22efeddbdf6fb43c72efbcf36ca36c27e3d71c2b
Matthis Kruse <s8makrus@stud.uni-saarland.de>
2020-11-05 15:44:29 +0100

    Change benchmark script.

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -9,18 +9,38 @@ max_threads=$(( max_threads / 2 ))
 mkdir -p bench
 cd bench
 
+backends="cpu"
+[[ -z "${ICONEX_BENCH}" ]] || backends="$backends $ICONEX_BENCH"
+
+echo "Benchmarking backends: $backends"
+
 # Run cpu based backends
-for backend in "cpu" "avx"; do
-  echo "------------------------ ${backend} -"
-  for t in $(seq $max_threads); do
+for backend in $backends; do
+  printf -- "--------------------- %6s -\n" "${backend}"
+
+  if [ $backend = "cpu" ] || [ $backend = "avx" ]; then
+    for t in $(seq $max_threads); do
+      cmake ../.. -DAnyDSL_runtime_DIR=$PWD/../../../runtime/build/share/anydsl/cmake/ \
+        -DBACKEND=$backend \
+        -DNO_OF_THREADS=$t >/dev/null 2>&1
+
+      make -j"$max_threads" >/dev/null 2>&1
+
+      printf -- "-- [Threads: %2s] -------------\n" "$t"
+
+      for div_function_name in 'generic' 'triangle'; do
+        echo "Div function name: ${div_function_name}"
+        { for _ in $(seq ${iter_count}); do
+            './global_div' "${grid}" "${lvl_count}" "${div_function_name}" || exit 1
+          done } | awk '/Operation time, ms: / {sum+=$4; count+=1}END{print "Total runs:", count, "\nAverage time:", sum/count, "ms"}' || exit 1
+        echo "------------------------------"
+      done
+    done
+  else
     cmake ../.. -DAnyDSL_runtime_DIR=$PWD/../../../runtime/build/share/anydsl/cmake/ \
-      -DBACKEND=$backend \
-      -DNO_OF_THREADS=$t >/dev/null 2>&1
+      -DBACKEND=$backend >/dev/null 2>&1
 
     make -j"$max_threads" >/dev/null 2>&1
-
-    # TODO: this assumes that $t is less than 10, otherwise we'll see ugly stdout :^)
-    echo "-- [Threads: $t] --------------"
 
     for div_function_name in 'generic' 'triangle'; do
       echo "Div function name: ${div_function_name}"
@@ -29,7 +49,7 @@ for backend in "cpu" "avx"; do
         done } | awk '/Operation time, ms: / {sum+=$4; count+=1}END{print "Total runs:", count, "\nAverage time:", sum/count, "ms"}' || exit 1
       echo "------------------------------"
     done
-  done
+  fi
 done
 
 cd ..
