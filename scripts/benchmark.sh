9424ca871dc757e7048ecbaa0311fbea61716293
Matthis Kruse <s8makrus@stud.uni-saarland.de>
2020-11-09 19:31:29 +0100

    Allow to set the number of times a benchmark is run.

--- scripts/benchmark.sh
+++ scripts/benchmark.sh
@@ -6,6 +6,9 @@ grid=src/icon_grid_0019_R02B05_G.nc
 lvl_count=40
 max_threads=$(nproc)
 max_threads=$(( max_threads / 2 ))
+[[ -z "${ICONEX_BENCH_NUM_ITER}" ]] && num_iter=1 || num_iter=$ICONEX_BENCH_NUM_ITER
+
+echo "Number of iterations per round: "$num_iter
 
 mkdir -p bench
 cd bench
@@ -17,12 +20,33 @@ echo -ne "Benchmarking backends: $backends\n\n"
 echo "backend median minimum maximum generic threads"
 # Run cpu based backends
 for backend in $backends; do
-  if [ $backend = "cpu" ] || [ $backend = "avx" ]; then
-    for t in $(seq $max_threads); do
+  for no in $(seq $num_iter); do
+    if [ $backend = "cpu" ] || [ $backend = "avx" ]; then
+      for t in $(seq $max_threads); do
+        cmake ../.. -DAnyDSL_runtime_DIR=$PWD/../../../runtime/build/share/anydsl/cmake/ \
+          -DCMAKE_BUILD_TYPE=release \
+          -DBACKEND=$backend \
+          -DNO_OF_THREADS=$t >/dev/null 2>&1
+
+        make -j"$max_threads" >/dev/null 2>&1
+
+        for div_function_name in 'generic' 'triangle'; do
+          echo -ne "$backend "
+
+          ANYDSL_PROFILE=full './global_div' "${grid}" "${lvl_count}" "${div_function_name}" | \
+            awk '/Timing: /''{ printf "%.2f %.2f %.2f ", $2, $4, $6 }' || exit 1
+
+          if [ $div_function_name = "generic" ]; then
+            echo -ne "1 "
+          else
+            echo -ne "0 "
+          fi
+          echo -ne "$t\n"
+        done
+      done
+    else
       cmake ../.. -DAnyDSL_runtime_DIR=$PWD/../../../runtime/build/share/anydsl/cmake/ \
-        -DCMAKE_BUILD_TYPE=release \
-        -DBACKEND=$backend \
-        -DNO_OF_THREADS=$t >/dev/null 2>&1
+        -DBACKEND=$backend >/dev/null 2>&1
 
       make -j"$max_threads" >/dev/null 2>&1
 
@@ -33,33 +57,14 @@ for backend in $backends; do
           awk '/Timing: /''{ printf "%.2f %.2f %.2f ", $2, $4, $6 }' || exit 1
 
         if [ $div_function_name = "generic" ]; then
-          echo -ne "1 "
+          echo -ne "1 1"
         else
-          echo -ne "0 "
+          echo -ne "0 1"
         fi
-        echo -ne "$t\n"
+        echo -ne "\n"
       done
-    done
-  else
-    cmake ../.. -DAnyDSL_runtime_DIR=$PWD/../../../runtime/build/share/anydsl/cmake/ \
-      -DBACKEND=$backend >/dev/null 2>&1
-
-    make -j"$max_threads" >/dev/null 2>&1
-
-    for div_function_name in 'generic' 'triangle'; do
-      echo -ne "$backend "
-
-      ANYDSL_PROFILE=full './global_div' "${grid}" "${lvl_count}" "${div_function_name}" | \
-        awk '/Timing: /''{ printf "%.2f %.2f %.2f ", $2, $4, $6 }' || exit 1
-
-      if [ $div_function_name = "generic" ]; then
-        echo -ne "1 1"
-      else
-        echo -ne "0 1"
-      fi
-      echo -ne "\n"
-    done
-  fi
+    fi
+  done
 done
 
 cd ..
